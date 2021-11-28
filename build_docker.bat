77b9ed3208260e254cc32d54e3afe55991ecbe94
Joshua Shreve <j.a.shreve@gmail.com>
2021-11-28 16:09:13 -0500

    Update build_docker.bat
    Fixed docker mount directories to include whole project to prevent the need to specifically call out any added directories.

--- build_docker.bat
+++ build_docker.bat
@@ -1,5 +1,5 @@
 docker build -t dactyl-keyboard -f docker/Dockerfile .
-docker run --name DM-run -d -v "%cd%/src:/app/src" -v "%cd%/things:/app/things" -v "%cd%/things:/app/configs"  dactyl-keyboard python3 -i dactyl_manuform.py
-docker run --name DM-config -d -v "%cd%/src:/app/src" -v "%cd%/things:/app/things" -v "%cd%/things:/app/configs" dactyl-keyboard python3 -i generate_configuration.py
-docker run --name DM-release-build -d -v "%cd%/src:/app/src" -v "%cd%/things:/app/things" -v "%cd%/things:/app/configs" dactyl-keyboard python3 -i model_builder.py
-docker run --name DM-shell -d -ti -v "%cd%/src:/app/src" -v "%cd%/things:/app/things"  -v "%cd%/things:/app/configs" dactyl-keyboard
+docker run --name DM-run -d -v "%cd%/:/app"  dactyl-keyboard python3 -i dactyl_manuform.py
+docker run --name DM-config -d -v "%cd%/:/app"  dactyl-keyboard python3 -i generate_configuration.py
+docker run --name DM-release-build -d -v "%cd%/:/app"  dactyl-keyboard python3 -i model_builder.py
+docker run --name DM-shell -d -ti -v "%cd%/:/app"  dactyl-keyboard
